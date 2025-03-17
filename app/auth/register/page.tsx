c28f84158eb32354f8fa05bb63f016035d7bd185
Victor Ramirez <vicdeveloper83@gmail.com>
2025-03-17 22:02:15 +0000

    set font in client component

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,20 +1,13 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import { Outfit } from 'next/font/google';
 import RegisterForm from '@/app/components/RegisterForm';
 
 const outfit = Outfit({ subsets: ['latin'] });
 
 export default function RegisterPage() {
-	const [isMounted, setIsMounted] = useState(false);
-
-	useEffect(() => {
-		setIsMounted(true);
-	}, []);
-
 	return (
-		<div className={isMounted ? outfit.className : ''}>
+		<div className={outfit.className}>
 			<h1 className='font-black text-6xl text-purple-950'>Create Account</h1>
 			<p className='text-3xl font-bold'>
 				Control your <span className='text-amber-500'>budgets</span>
